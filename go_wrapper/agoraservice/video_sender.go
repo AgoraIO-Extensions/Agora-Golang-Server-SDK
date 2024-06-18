aa32c089cc4fd0a3c40e500a1e7fe09140d8fc5e
lihuize <lihuize@agora.io>
2024-06-18 19:52:32 +0800

    [FIX] make video send and recv work

--- go_wrapper/agoraservice/video_sender.go
+++ go_wrapper/agoraservice/video_sender.go
@@ -9,9 +9,21 @@ package agoraservice
 #include "agora_rtc_conn.h"
 #include "agora_service.h"
 #include "agora_media_base.h"
+#include "agora_video_track.h"
 */
 import "C"
 import "unsafe"
+
+type VideoEncoderConfig struct {
+	CodecType         int
+	Width             int
+	Height            int
+	Framerate         int
+	Bitrate           int
+	MinBitrate        int
+	OrientationMode   int
+	DegradePreference int
+}
 
 type VideoSender struct {
 	// config       *PcmSenderConfig
@@ -47,12 +59,29 @@ func (con *RtcConnection) ReleaseVideoSender() {
 	con.videoSender = nil
 }
 
+func (sender *VideoSender) SetVideoEncoderConfig(cfg *VideoEncoderConfig) int {
+	cCfg := C.struct__video_encoder_config{}
+	C.memset(unsafe.Pointer(&cCfg), 0, C.sizeof_struct__video_encoder_config)
+	cCfg.codec_type = C.int(cfg.CodecType)
+	cCfg.dimensions.width = C.int(cfg.Width)
+	cCfg.dimensions.height = C.int(cfg.Height)
+	cCfg.frame_rate = C.int(cfg.Framerate)
+	cCfg.bitrate = C.int(cfg.Bitrate)
+	cCfg.min_bitrate = C.int(cfg.MinBitrate)
+	cCfg.orientation_mode = C.int(cfg.OrientationMode)
+	cCfg.degradation_preference = C.int(cfg.DegradePreference)
+	return int(C.agora_local_video_track_set_video_encoder_config(sender.cVideoTrack, &cCfg))
+}
+
 func (sender *VideoSender) Start() int {
+	C.agora_local_video_track_set_enabled(sender.cVideoTrack, C.int(1))
 	return int(C.agora_local_user_publish_video(sender.cLocalUser, sender.cVideoTrack))
 }
 
 func (sender *VideoSender) Stop() int {
-	return int(C.agora_local_user_unpublish_video(sender.cLocalUser, sender.cVideoTrack))
+	ret := int(C.agora_local_user_unpublish_video(sender.cLocalUser, sender.cVideoTrack))
+	C.agora_local_video_track_set_enabled(sender.cVideoTrack, C.int(0))
+	return ret
 }
 
 func (sender *VideoSender) SendVideoFrame(frame *VideoFrame) int {
