16bdb2a843471189998c048ea7fb50fd048f1344
lihuize <lihuize@agora.io>
2024-06-25 12:23:40 +0800

    [FEAT] make linux vad work

--- go_wrapper/agoraservice/video_sender.go
+++ go_wrapper/agoraservice/video_sender.go
@@ -3,7 +3,7 @@ package agoraservice
 /*
 #cgo CFLAGS: -I../../agora_sdk/include_c/api2 -I../../agora_sdk/include_c/base
 #cgo darwin arm64 LDFLAGS: -L../../agora_sdk_mac/arm64 -lAgoraRtcKit -lAgorafdkaac -lAgoraffmpeg
-#cgo linux LDFLAGS: -L../../agora_sdk/ -lagora_rtc_sdk -lagora-fdkaac -lagora-ffmpeg
+#cgo linux LDFLAGS: -L../../agora_sdk/ -lagora_rtc_sdk -lagora-fdkaac -lagora-core
 
 #include <string.h>
 #include "agora_local_user.h"
