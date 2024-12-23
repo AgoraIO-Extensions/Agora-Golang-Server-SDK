97f83ca183c6830da198d03ece4c20065be969cf
weihongqin <weihongqin@agora.io>
2024-12-23 18:56:23 +0800

    UPDATE: update sdk version from 4.4.30 to 4.4.31

--- scripts/install_agora_sdk.sh
+++ scripts/install_agora_sdk.sh
@@ -7,9 +7,12 @@ PACKAGE_HOME=$(
 
 UNAME_S=`uname -s`
 OS=unknown
-
-linux_sdk="https://download.agora.io/sdk/release/agora_rtc_sdk-x86_64-linux-gnu-v4.4.30-20241024_101940-398537.zip"
-mac_sdk="https://download.agora.io/sdk/release/agora_rtc_sdk_mac_rel.v4.4.30_22472_FULL_20241024_1224_398653.zip"
+# 目前自动解析有问题，测试发现是打包的问题，可以通过人工来执行查看差异点
+linux_sdk="https://download.agora.io/sdk/release/agora_rtc_sdk-x86_64-linux-gnu-v4.4.31-20241223_111509-491956.zip"
+mac_sdk="https://download.agora.io/sdk/release/agora_sdk_mac_v4.4.31_23136_FULL_20241223_1245_492039.zip"
+# old version
+#linux_sdk="https://download.agora.io/sdk/release/agora_rtc_sdk-x86_64-linux-gnu-v4.4.30-20241024_101940-398537.zip"
+#mac_sdk="https://download.agora.io/sdk/release/agora_rtc_sdk_mac_rel.v4.4.30_22472_FULL_20241024_1224_398653.zip"
 
 if [[ $UNAME_S == Linux ]]; then
     OS=linux
