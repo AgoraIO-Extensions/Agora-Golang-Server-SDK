3b96becf641abdf3fbf560182338090fcbf7374a
weihongqin <weihongqin@agora.io>
2025-05-15 20:28:37 +0800

    Add: set loglevel to error for test

--- go_sdk/agoraservice/agora_service.go
+++ go_sdk/agoraservice/agora_service.go
@@ -184,6 +184,7 @@ func Initialize(cfg *AgoraServiceConfig) int {
 		}
 		C.agora_service_set_log_file(agoraService.service, logPath,
 			C.uint(logSize))
+		C.agora_service_set_log_filter(agoraService.service, 12)
 	}
 	agoraService.inited = true
 	return 0
