d33ace0bdbf74cc88373632947f9ae31ef2acf12
weihongqin <weihongqin@agora.io>
2025-07-04 20:21:43 +0800

    move pub/unpub to con, and move InterruptAudio to con

--- go_sdk/agoraservice/local_user.go
+++ go_sdk/agoraservice/local_user.go
@@ -13,14 +13,12 @@ package agoraservice
 */
 import "C"
 import (
-	"fmt"
 	"unsafe"
 )
 
 type LocalUser struct {
 	connection *RtcConnection
 	cLocalUser unsafe.Pointer
-	audioTrack *LocalAudioTrack
 	publishFlag bool
 }
 
@@ -184,21 +182,15 @@ func (localUser *LocalUser) SetAudioEncoderConfiguration(config *AudioEncoderCon
 // Implement the publication of audio track
 
 
-func (localUser *LocalUser) PublishAudio(track *LocalAudioTrack) int {
+func (localUser *LocalUser) publishAudio(track *LocalAudioTrack) int {
 	if localUser.cLocalUser == nil {
 		return -1
 	}
-	if localUser.audioTrack == nil {
-		localUser.audioTrack = track
-	} 
-	//fmt.Printf("______publish audio track id %d, old id %d, cTrack %p, old cTrack %p\n", track.id, localUser.audioTrack.id, track.cTrack, localUser.audioTrack.cTrack)
 
 	if localUser.publishFlag == true {
 	    return 0
 	}
 	
-	// 将内部保持的cTrack赋值给track
-	track.cTrack = localUser.audioTrack.cTrack
 	ret := int(C.agora_local_user_publish_audio(localUser.cLocalUser, track.cTrack))
 	localUser.publishFlag = true
 	if ret != 0 {
@@ -208,84 +200,9 @@ func (localUser *LocalUser) PublishAudio(track *LocalAudioTrack) int {
 	
 }
 
-//TODO: 这样修改后，应用层也会被同步修改！ 
-//ToDo: 提供一个打断api，设置在localUser::InterruptAudio()
-//
-// 内部只是做原子+正交化原则设计api
-// 应用层需要做：打断操作！
-// 然后调用UpdateAudioTrack
-// 然后在做pcmsender的更新senario
-// 然后调用PublishAudio
-func (localUser *LocalUser) UpdateAudioSenario(senario AudioScenario) int {
-	if localUser.cLocalUser == nil {
-		return -1
-	}
-	// check if the track is the same as the internal one
-	if localUser.audioTrack == nil {	
-		return 0
-	}
-	// update the connection's senario
-	if localUser.connection.audioScenario != senario {
-		localUser.connection.audioScenario = senario
-		localUser.SetAudioScenario(senario)
-	}
-	
-	// 如果senario是一样的，则不做任何操作
-	if localUser.audioTrack.audioScenario == senario {
-		return 0
-	}
-	// 如果senario不一样，则释放cTrack,重新创建一个新的cTrack，然后做assign
 
-	//1. disable,and unpublish the track
-	localUser.audioTrack.SetEnabled(false)
-	localUser.UnpublishAudio(localUser.audioTrack)
-	//2. never change the id of the track,but only release the cTrack
-	localUser.audioTrack.Release()
 
-	//3. update the audioScenario
-	localUser.audioTrack.audioScenario = senario
-	localUser.audioTrack.cTrack = nil
-	//ToDo：回退交给客户来做会更好，用最佳实践的方式来
-	//原因是因为客户选择的回退策略，不一定是chorus，而是其他策略！！这样我们固定策略，会限制客户
-	//4. create a new cTrack
-	var cTrack unsafe.Pointer = nil
-	var isAiServer bool = false
-	if senario == AudioScenarioAiServer {
-		isAiServer = true
-	}
-	if isAiServer {
-		cTrack  = C.agora_service_create_direct_custom_audio_track_pcm(agoraService.service, localUser.audioTrack.pcmSender.cSender)
-	} else {
-		cTrack = C.agora_service_create_custom_audio_track_pcm(agoraService.service, localUser.audioTrack.pcmSender.cSender)
-	}
-
-	//5. assign the new cTrack
-	localUser.audioTrack.cTrack = cTrack
-
-	//6. set properties to new cTrack
-	localUser.audioTrack.SetSendDelayMs(10)
-	if isAiServer == false {
-		localUser.audioTrack.SetMaxBufferedAudioFrameNumber(100000) //up to 16min,100000 frames
-	}
-	localUser.SetAudioScenario(senario)
-
-	//7. update the properties of pcmsender
-	//update pcmsender's info
-	localUser.audioTrack.pcmSender.audioScenario = senario
-	//update connection's info
-	localUser.connection.audioScenario = senario
-
-	//8. publish the track
-	localUser.audioTrack.SetEnabled(true)
-	localUser.PublishAudio(localUser.audioTrack)
-	
-	fmt.Printf("______update audio track id %d, old id %d, cTrack %p, old cTrack %p\n", localUser.audioTrack.id, localUser.audioTrack.id, localUser.audioTrack.cTrack, localUser.audioTrack.cTrack)
-	
-	
-	return 0
-}
-
-func (localUser *LocalUser) UnpublishAudio(track *LocalAudioTrack) int {
+func (localUser *LocalUser) unpublishAudio(track *LocalAudioTrack) int {
 	if localUser.cLocalUser == nil {
 		return -1
 	}
@@ -301,14 +218,14 @@ func (localUser *LocalUser) UnpublishAudio(track *LocalAudioTrack) int {
 	return ret
 }
 
-func (localUser *LocalUser) PublishVideo(track *LocalVideoTrack) int {
+func (localUser *LocalUser) publishVideo(track *LocalVideoTrack) int {
 	if localUser.cLocalUser == nil {
 		return -1
 	}
 	return int(C.agora_local_user_publish_video(localUser.cLocalUser, track.cTrack))
 }
 
-func (localUser *LocalUser) UnpublishVideo(track *LocalVideoTrack) int {
+func (localUser *LocalUser) unpublishVideo(track *LocalVideoTrack) int {
 	if localUser.cLocalUser == nil {
 		return -1
 	}
@@ -366,29 +283,4 @@ func (localUser *LocalUser) SendAudioMetaData(metaData []byte) int {
 	
 	ret := C.agora_local_user_send_audio_meta_data(localUser.cLocalUser, (*C.char)(cMetaData), (C.size_t)(len(metaData)))
 	return int(ret)
-}
-// add a api to interrupt the audio
-func (localUser *LocalUser) InterruptAudio(audioConsumer *AudioConsumer) int {
-	if localUser.cLocalUser == nil {
-		return -1
-	}
-	if audioConsumer != nil {
-		audioConsumer.Clear()
-	}
-	
-	
-	// 根据不同的audioScenario, 做不同的处理
-	
-	if localUser.audioTrack.audioScenario == AudioScenarioAiServer {
-		// for aiServer, we need to unpublish the track
-		localUser.UnpublishAudio(localUser.audioTrack)
-
-		// and publish the track again
-		localUser.PublishAudio(localUser.audioTrack)
-		
-	} else {
-		// and other scenarios, we need to clear the buffer of the track
-		localUser.audioTrack.ClearSenderBuffer()
-	}
-	return 0
 }
