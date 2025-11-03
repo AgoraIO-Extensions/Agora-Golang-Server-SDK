29110531aa8a131037f20c488b6e0348d77143b7
weihongqin <weihongqin@agora.io>
2025-11-03 16:35:07 +0800

    add: add timer support

--- go_sdk/rtc/agora_utils.go
+++ go_sdk/rtc/agora_utils.go
@@ -676,3 +676,42 @@ func GenerateWAVHeader(sampleRate int, channels int, pcmDataSizeInBytes int) []b
 	binary.LittleEndian.PutUint32(header[40:44], uint32(pcmDataSizeInBytes))
 	return header
 }
+
+type PrecisionTimer struct {
+    ticker   *time.Ticker
+    stopChan chan bool
+}
+
+func NewPrecisionTimer(interval int) *PrecisionTimer {
+    return &PrecisionTimer{
+        ticker:   time.NewTicker(time.Duration(interval) * time.Millisecond),
+        stopChan: make(chan bool),
+    }
+}
+func (t *PrecisionTimer) Start(taskFunc func()) {
+    go func() {
+        for {
+            select {
+            case <-t.ticker.C:
+                taskFunc()
+            case <-t.stopChan:
+                t.ticker.Stop()
+                return
+            }
+        }
+    }()
+}
+func (pt *PrecisionTimer) Stop() {
+    pt.ticker.Stop()
+    close(pt.stopChan)
+}
+type IdleItem struct {
+	Handle unsafe.Pointer
+	LifeCycle int // in ms
+}
+func newIdleItem(handle unsafe.Pointer, lifeCycle int) *IdleItem {
+    return &IdleItem{
+        Handle: handle,
+        LifeCycle: lifeCycle,
+    }
+}
