b0064c88fd3b91740e63a6d9acbec797b8dbce45
weihongqin <weihongqin@agora.io>
2025-11-26 18:10:35 +0800

    change: change sample

--- go_sdk/examples/example_externalaudio_processor/example_externalaudio_processor.go
+++ go_sdk/examples/example_externalaudio_processor/example_externalaudio_processor.go
@@ -10,7 +10,6 @@ import (
 	"os/signal"
 	"runtime"
 	"strconv"
-	"sync"
 	"time"
 
 	agoraservice "github.com/AgoraIO-Extensions/Agora-Golang-Server-SDK/v2/go_sdk/rtc"
@@ -18,143 +17,8 @@ import (
 )
 
 
-func PushFileToConsumer(file *os.File, con *agoraservice.RtcConnection, samplerate int) {
-	buffer := make([]byte, samplerate*2) // 1s data
-	for {
-		readLen, err := file.Read(buffer)
-		if err != nil {
-			fmt.Printf("read up to EOF,cur read: %d", readLen)
-			file.Seek(0, 0)
-			return
-		}
-		// round to integer of chunk
-		packLen := readLen / samplerate
-		con.PushAudioPcmData(buffer[:packLen*samplerate], samplerate, 1, 0)
-		fmt.Printf("PushPCMData done:%d\n", readLen)
-	}
-}
-func ReadFileToConsumer(file *os.File, con *agoraservice.RtcConnection, interval int, done chan bool, samplerate int) {
-	for {
-		select {
-		case <-done:
-			fmt.Println("ReadFileToConsumer done")
-			return
-		default:
-			if con != nil {
-				isPushCompleted := con.IsPushToRtcCompleted()
-				if isPushCompleted {
-					PushFileToConsumer(file, con, samplerate)
-				}
-			}
-			time.Sleep(time.Duration(interval) * time.Millisecond)
-		}
-	}
-}
 
-
-
-/*
-date:2025-08-21
-author: weihongqin
-description: byte manager for pcm raw data
-*/
-
-// ByteManager: the manager for pcm raw data
-type PcmRawDataManager struct {
-	data       []byte
-	start      int // the start position of the buffer
-	lock       sync.Mutex
-	sampleRate int // the sample rate of the pcm data
-	channels   int // the channels of the pcm data
-	bytesInMs  int // the bytes in ms of the pcm data
-}
-
-// NewByteManager: create a new byte manager
-func NewPcmRawDataManager(sampleRate int, channels int) *PcmRawDataManager {
-	bm := &PcmRawDataManager{
-		data:       make([]byte, 0),
-		sampleRate: 24000,
-		channels:   1,
-		bytesInMs:  0,
-	}
-	bm.sampleRate = sampleRate
-	bm.channels = channels
-	bm.bytesInMs = sampleRate * 2 / 1000 * channels
-	return bm
-}
-
-// UpdateParameters: update the parameters of the byte manager
-// return 0: success
-// return 1: if the parameters are the same as the current parameters, do nothing
-func (bm *PcmRawDataManager) UpdateParameters(sampleRate int, channels int) int {
-	bm.lock.Lock()
-	defer bm.lock.Unlock()
-	if sampleRate != bm.sampleRate || channels != bm.channels {
-		bm.sampleRate = sampleRate
-		bm.channels = channels
-		bm.bytesInMs = sampleRate * 2 / 1000 * channels
-		// and reset the data
-		bm.Reset()
-		return 0
-	}
-	return 1
-}
-
-// Push: push data to the end of the buffer without copy
-// always push the data to the end of the buffer
-func (bm *PcmRawDataManager) Push(data []byte) {
-	bm.lock.Lock()
-	defer bm.lock.Unlock()
-	bm.data = append(bm.data, data...)
-}
-
-// Pop: pop data from the begin of the buffer without copy
-// the returned slice is the view of the internal data, and the caller should not modify it
-// if the available data is not round to the nearest multiple of bytesInMs, return nil
-func (bm *PcmRawDataManager) Pop() []byte {
-	bm.lock.Lock()
-	defer bm.lock.Unlock()
-
-	available := len(bm.data) - bm.start
-	// round to the nearest multiple of bytesInMs
-	length := available / bm.bytesInMs * bm.bytesInMs
-	// if length is 0, return empty slice
-	if length == 0 {
-		return nil
-	}
-
-	// return the data view of the requested length
-	result := bm.data[bm.start : bm.start+length]
-	bm.start += length
-
-	// 如果已弹出超过一半的数据，进行内存整理
-	if bm.start > len(bm.data)/2 {
-		bm.data = bm.data[bm.start:]
-		bm.start = 0
-	}
-
-	return result
-}
-
-// Len : return the data length in the buffer
-func (bm *PcmRawDataManager) Len() int {
-	bm.lock.Lock()
-	defer bm.lock.Unlock()
-	return len(bm.data) - bm.start
-}
-
-// Reset: reset the buffer or empty the buffer
-// and return the remain data length
-func (bm *PcmRawDataManager) Reset() int {
-	bm.lock.Lock()
-	defer bm.lock.Unlock()
-	remain := len(bm.data) - bm.start
-	bm.data = bm.data[:0]
-	bm.start = 0
-	return remain
-}
-
-
+//mode: 0: only vad, 1: apm+vad, 2: apm+vad+dump
 func main() {
 	bStop := new(bool)
 	*bStop = false
@@ -173,7 +37,7 @@ func main() {
 	}()
 	
 
-	println("Start to audio processor\nusage:\n	./example_externalaudio_processor <appid> <channel_name> <outsamplerate> <outputchannels> \n	press ctrl+c to exit\n")
+	println("Start to audio processor\nusage:\n	./example_externalaudio_processor <appid> <channel_name> <outsamplerate> <outputchannels> <mode> \n	press ctrl+c to exit\n")
 
 	// get parameter from arguments： appid, channel_name
 
@@ -187,6 +51,7 @@ func main() {
 	filepath := ""
 	inputsamplerate := 16000
 	inputchannels := 1
+	mode := 0
 
 	if len(argus) > 3 {
 		filepath = argus[3]
@@ -196,6 +61,9 @@ func main() {
 	}
 	if len(argus) > 5 {
 		inputchannels, _ = strconv.Atoi(argus[5])
+	}
+	if len(argus) > 6 {
+		mode, _ = strconv.Atoi(argus[6])
 	}
 	
 	fmt.Println("appid:", appid, "channelName:", channelName, "filepath:", filepath, "inputsamplerate:", inputsamplerate, "inputchannels:", inputchannels)
@@ -211,9 +79,13 @@ func main() {
 	svcCfg.ConfigDir = "./agora_rtc_log"
 	svcCfg.DataDir = "./agora_rtc_log"
 	svcCfg.IdleMode = true
-	svcCfg.APMModel = 2
+	svcCfg.APMModel = 1
 	svcCfg.APMConfig = agoraservice.NewAPMConfig()
-	svcCfg.APMConfig.EnableDump = true
+	svcCfg.APMConfig.EnableDump = false
+
+	if mode == 2 {
+		svcCfg.APMConfig.EnableDump = true
+	}
 
 	//step2: initialize the agora service
 	agoraservice.Initialize(svcCfg)
@@ -223,14 +95,42 @@ func main() {
 
 	//step4: initialize the external audio processor
 	apmConfig := agoraservice.NewAPMConfig()
-	apmConfig.EnableDump = true
+	apmConfig.EnableDump = false
 	apmConfig.AiAecConfig.Enabled = false
 	apmConfig.BghvsCConfig.Enabled = true
 	apmConfig.AgcConfig.Enabled = false
 	apmConfig.AiNsConfig.AiNSEnabled = true
 	apmConfig.AiNsConfig.NsEnabled = true
 	apmConfig.BghvsCConfig.Enabled = true
-	ret := externalAudioProcessor.Initialize(apmConfig, inputsamplerate, inputchannels, nil)
+	
+	vadConfig := &agoraservice.AudioVadConfigV2{
+		EnableAdaptiveRmsThreshold: true,
+		AdaptiveRmsThresholdFactor: 0.67,
+		StartVoiceProb: 70,
+		StartRms: -50,
+		StopVoiceProb: 70,
+		StopRms: -50,
+	}
+
+	// add observer
+	observer := &agoraservice.ExternalAudioProcessorObserver{
+		OnProcessedAudioFrame: func(frame *agoraservice.AudioFrame, vadResultStat agoraservice.VadState, vadResultFrame *agoraservice.AudioFrame) {
+			var retBufferLen int = 0
+			if vadResultFrame != nil {
+				retBufferLen = len(vadResultFrame.Buffer)
+			}
+			fmt.Printf("OnProcessedAudioFrame, voice prob: %d, rms: %d, pitch: %d, vadResultStat: %d, vadResultFrame len: %d\n", frame.VoiceProb, frame.Rms, frame.Pitch, vadResultStat, retBufferLen)
+		},
+	}
+
+
+	// mode: 0: only vad, so set apmconfig to nil
+	if mode == 0 {
+		apmConfig = nil
+	}
+	
+
+	ret := externalAudioProcessor.Initialize(apmConfig, inputsamplerate, inputchannels, vadConfig,observer)
 	if ret != 0 {
 		fmt.Printf("Failed to initialize external audio processor, error code: %d\n", ret)
 	}
@@ -282,7 +182,8 @@ func main() {
 		}
 		//time.Sleep(10 * time.Millisecond)
 	}
-	fmt.Printf("Total length: %d, packs: %d, total time: %dms\n", totalLen, totalInPacks, time.Now().UnixMilli() - startTime)
+	fmt.Printf("Total length: %d, packs: %d, total time: %dms, inStreamInMs: %d, processedStreamInMs: %d\n", totalLen, totalInPacks, time.Now().UnixMilli() - startTime,
+externalAudioProcessor.InStreamInMs, externalAudioProcessor.ProcessedStreamInMs)
 
 	externalAudioProcessor.Release()
 
